60095fcc16568e8e6b23cf1171e63d4cc5d65cd1
admin <admin@192.168.160.1>
2023-02-14 13:20:29 +0530

    commit 1

---
+++ ActitimeTesting/src/test/java/com/testActitime/testscripts/DemoTest.java
@@ -0,0 +1,14 @@
+package com.testActitime.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void testDemo()
+	{
+		Reporter.log("testDemo", true);
+	}
+
+}
